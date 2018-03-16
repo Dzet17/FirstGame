247ef72df1f2c163b7d003b678a3456e3ac6071c
Dylan Zettler <dylanzettler@gmail.com>
2018-03-16 08:40:07 -0600

    Created an if loop for the run constructor

--- src/com/main/Game.java
+++ src/com/main/Game.java
@@ -8,6 +8,9 @@ public class Game extends Canvas implements Runnable
 
 	public static final int WIDTH = 640, HEIGHT = WIDTH / 12 * 9;
 	
+	private Thread thread;
+	private boolean running = false;
+	
 	public Game()
 	{
 		new Window(WIDTH, HEIGHT, "My First Game", this);
@@ -15,10 +18,49 @@ public class Game extends Canvas implements Runnable
 	
 	
 	public synchronized void start() {
+		thread = new Thread(this);
+		thread.start();
+		running = true;
+	}
+	
+	public synchronized void stop(){
+		try{
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}	
+	public void run() {
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		while(running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while(delta >= 1){
+				tick();
+				delta--;
+			}
+			if(running)
+				render();
+			frames++;
+			
+			if(System.currentTimeMillis() - timer > 1000){
+				timer += 1000;
+				System.out.println(("FPS: " + frames);
+				frames = 0;
+			}
+		}
+		stop();
+	}
+	
+	private void tick() {
 		
 	}
 	
-	public void run() {
+	private void render() {
 		
 	}
 	

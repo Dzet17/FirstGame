923e6c16442b15b12cbbf6616548887fefac5d36
Dylan Zettler <dylanzettler@gmail.com>
2018-03-20 09:18:40 -0600

    Player and Enemy enums created, will now create GameObjects connected to them

--- src/com/main/GameObject.java
+++ src/com/main/GameObject.java
@@ -1,6 +1,18 @@
 package com.main;
 
-public class GameObject
+import java.awt.Graphics;
+
+public abstract class GameObject
 {
-protected int x, y;
+	protected int x, y;
+	protected ID id;
+	protected int volX, volY;
+
+	public GameObject(int x, int y, ID id) {
+	this.x = x;
+	this.y = y;
+	this.id = id;
+	}
+	public abstract void tick();
+	public abstract void render(Graphics g);
 }
--- src/com/main/ID.java
+++ src/com/main/ID.java
@@ -2,5 +2,6 @@ package com.main;
 
 public enum ID
 {
-
+	Player(),
+	Enemy();
 }
